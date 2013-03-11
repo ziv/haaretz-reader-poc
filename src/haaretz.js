3d83bbf9b8c0290779437fce8e78b3be4811da88
zivperry <ziv.perry@gmail.com>
2013-03-11 12:49:27 +0200

    first time

---
+++ src/haaretz.js
@@ -0,0 +1,45 @@
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function(info) {
+		
+		// remove referer header if exists
+		for (var i = 0; i < info.requestHeaders.length; ++i) {
+			if (info.requestHeaders[i].name === "Referer") {
+				info.requestHeaders.splice(i, 1);
+				break;
+			}
+		}
+		//console.log(info.requestHeaders);
+		
+		// choose referer url 
+		// TODO: add more URLs that Haaretz support
+		var list	= ["http://www.facebook.com/", "http://www.twitter.com/"];
+		var url		= list[ Math.round(Math.random() * 1) ];
+		
+		// create salt
+		// TODO: is referer refer to full URI? or only scheme and domain?
+		var len		= Math.round(Math.random() * 7);
+		var chars	= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		var salt	= "";
+		
+		for (var i = 0; i < len; ++i) {
+			salt += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		url += salt;
+		//console.log(url);
+
+		// add referer
+		info.requestHeaders.push({
+			name	: "Referer",
+			value	: url
+		});
+		//console.log(info.requestHeaders);
+		
+		return { requestHeaders: info.requestHeaders };
+	},
+	{
+		urls: ["*://*.haaretz.co.il/*"],
+		types: ["main_frame"]
+	},
+	["blocking", "requestHeaders"]);
