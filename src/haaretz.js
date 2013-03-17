23ac0fca8ed1f05c9becd76ed347a1913bebcc61
zivperry <ziv.perry@gmail.com>
2013-03-17 13:45:55 +0200

    Url change

--- src/haaretz.js
+++ src/haaretz.js
@@ -13,27 +13,12 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 		//console.log(info.requestHeaders);
 		
 		// choose referer url 
-		// TODO: add more URLs that Haaretz support
-		var list	= ["http://www.facebook.com/", "http://www.twitter.com/"];
-		var url		= list[ Math.round(Math.random() * 1) ];
+		// TODO: add more URLs that Haaretz support 
+		// TODO: Do we need those URLs or can we stick with one only?
+		// var list	= ["http://www.facebook.com/", "http://www.twitter.com/", "http://www.google.com"];
+		// var url		= list[ Math.round(Math.random() * 2) ];
 		
-		
-		
-		/*
-		// TODO: Do we need to salt to URL? - Answer: NO!
-		// create salt
-		var len		= Math.round(Math.random() * 7);
-		var chars	= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		var salt	= "";
-		
-		for (var i = 0; i < len; ++i) {
-			salt += chars.charAt(Math.floor(Math.random() * chars.length));
-		}
-		url += salt;
-		//console.log(url);
-		*/
-		
-		
+		var url		= "http://www.facebook.com/";
 		
 		// add referer
 		info.requestHeaders.push({
