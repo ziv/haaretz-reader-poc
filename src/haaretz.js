775a9bf374b6f00a6544858b16f806ac5bf3b6a0
Ziv Perry <ziv.perry@gmail.com>
2017-10-17 12:11:38 +0300

    es6

--- src/haaretz.js
+++ src/haaretz.js
@@ -1,28 +1,12 @@
 
-// Add a listener to any Haaretz request
-chrome.webRequest.onBeforeSendHeaders.addListener(
-	function(info) {
-		
-		// remove referer header if exists
-		for (var i = 0; i < info.requestHeaders.length; ++i) {
-			if (info.requestHeaders[i].name === "Referer") {
-				info.requestHeaders.splice(i, 1);
-				break;
-			}
-		}
-		
-		// add referer (facebook in this case, but can be any social network or search engine that Haaretz suports)
-		info.requestHeaders.push({
-			name	: "Referer",
-			value	: "http://www.facebook.com/"
-		});
-		
-		return { requestHeaders: info.requestHeaders };
-	},
-	{
-		// permisions
-		urls: ["*://*.haaretz.co.il/*"],
-		types: ["main_frame"]
-	},
-	// this is a blocking function, to change the request
-	["blocking", "requestHeaders"]);
+// TODO: add support for firefox
+
+const listener = info => {
+    let requestHeaders = info.requestHeaders.filter(i => i.name === 'Referer');
+    requestHeaders.push({name: 'Referer', value: 'http://www.facebook.com/'});
+    return {requestHeaders};
+};
+const permissions = {urls: ['*://*.haaretz.co.il/*'], types: ['main_frame']};
+const opts = ['blocking', 'requestHeaders'];
+
+chrome.webRequest.onBeforeSendHeaders.addListener(listener, permissions, opts);
