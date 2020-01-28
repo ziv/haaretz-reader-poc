9cdf957da258879b03af4ed81e51f6e6363aae42
Ziv Perry <ziv.perry@gmail.com>
2020-01-28 16:58:42 +0200

    change use of user agent instead of referrer

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-
 // TODO: add support for firefox
 
-const listener = info => {
-    let requestHeaders = info.requestHeaders.filter(i => i.name === 'Referer');
-    requestHeaders.push({name: 'Referer', value: 'http://www.facebook.com/'});
-    return {requestHeaders};
+const GOOGLE_BOT_USER_AGENT = {
+    name: 'User-Agent',
+    value: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 };
-const permissions = {urls: ['*://*.haaretz.co.il/*', '*://*.themarker.com'], types: ['main_frame']};
+
+const permissions = {urls: ['https://*.haaretz.co.il/*', 'https://*.themarker.com/*'], types: ['main_frame']};
+
 const opts = ['blocking', 'requestHeaders'];
 
-chrome.webRequest.onBeforeSendHeaders.addListener(listener, permissions, opts);
+const filterUserAgent = header => header.name.toLowerCase() !== 'user-agent';
+
+const onBeforeSendHeaders = ({requestHeaders}) => {
+    requestHeaders = requestHeaders.filter(filterUserAgent);
+    requestHeaders.push(GOOGLE_BOT_USER_AGENT);
+    console.log(requestHeaders);
+    return {requestHeaders};
+};
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, permissions, opts);
